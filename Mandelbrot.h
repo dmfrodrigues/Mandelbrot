6bc481bbdc9b42c737a9a081b00fe85ae9e21800
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-05-14 15:29:18 +0100

    Revert "Revert "remove comments""
    This reverts commit f540e5075ebfee390346326555bdaa2474111029.

--- Mandelbrot.h
+++ Mandelbrot.h
@@ -52,42 +52,5 @@ public:
     void GetColor(const std::deque<unsigned long int>& ChangedPixelsDeque);
 
 };
-/*
-namespace mb{
-    typedef long double ComplexT;
-    typedef long double ZoomT;
-    typedef long double StepT;
-    typedef int IterationT;
-    typedef float ColorT;
 
-    class ComplexNum{
-    public:
-        mb::ComplexT Re;
-        mb::ComplexT Im;
-        inline mb::ComplexT absSqr() const{
-            return (Re*Re + Im*Im);
-        };
-        inline bool isCardioid_isPeriod2Bulb() const{
-            mb::ComplexT q = (Re-0.25)*(Re-0.25) + Im*Im;    ///isCardioid
-            return (4.0*q*(q+(Re-0.25)) < Im*Im)||       ///isCardioid
-                   ((Re+1.0)*(Re+1.0) + Im*Im < 0.0625); ///isPeriod2Bulb
-        }
-    };
-
-    class Info{
-    public:
-        mb::ComplexNum c;
-        mb::ComplexNum z;
-        mb::IterationT it;
-        bool Check;
-        Info(mb::ComplexNum c_ = {0.0L,0.0L}, mb::ComplexNum z_ = {0.0L,0.0L},
-             mb::IterationT it_ = 0, bool Check_ = true);
-        void Reset();
-    };
-
-    ///CALCULATION ==========================================================================
-    unsigned long int GetPixels(wxNativePixelData& pixelData, wxSize sz,
-                                mb::IterationT numberIt, std::vector<mb::Info>& infoVtr);
-}
-*/
 #endif // MANDELBROT_H_INCLUDED

18cd4c0342975d3db1868af9d0da8c013f56ab32
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-05-28 16:42:31 +0100

    medir velocidade

--- tst/mb01.cpp
+++ tst/mb01.cpp
@@ -1,10 +1,19 @@
 #include "Mandelbrot.h"
 
+#include <chrono>
+#include <iostream>
+
+typedef std::chrono::high_resolution_clock hrclock;
+
 int main(){
     mb *f = new mb();
-    f->New({-1.25L,0.0L}, 1.0L, wxSize(700,400), 2.5L, true);
+    f->New({-1.375L,0.0L}, 20.0L, wxSize(2000,1000), 2.5L, true);
+    auto t1 = hrclock::now();
     for(int i = 0; i < 10; ++i){
-        f->UpdateMath(100);
+        f->UpdateMath(200);
     }
+    auto t2 = hrclock::now();
+    auto dt = std::chrono::duration<long double>(t2-t1);
+    std::cout << dt.count() << std::endl;
     return 0;
 }
